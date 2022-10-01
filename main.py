78ec2577afb514c856d43f26ab38dfdf5bc42924
Gaston Tomas Huete <gaston.tomas.huete.1997@gmail.com>
2022-10-02 00:55:57 +0200

    input the adventurer's name in "1" line

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # Autor: Gastán Tomás Huete
 # Date: 18/09/2022
+
+# variables globales a las que accedera el usuario
+gender = input("Que eres, ¿hombre o mujer?: ")
+name = ""
+
+if gender == "hombre".lower():
+    name = input("Muy bien, bienvenido... como deberias llamarte viajero? -> ")
+
+if gender == "mujer".lower():
+    name = input("Muy bien, bienvenida... como deberias llamarte viajera? -> ")
+
+# nombre ya inputeado
+print("Muy bien " + name + ", seras el ", end="")
+print("heroe", end="") if gender == "hombre".lower() else print("heroina", end="")
+print(" de esta leyenda.")
